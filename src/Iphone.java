e4fd1e3af540791580d4deacc1962e1d697f7950
Arhur Luis Oliveira Girão <agirao54@gmail.com>
2023-12-20 16:18:33 -0300

    Iphone 2007 ARQ

---
+++ src/Iphone.java
@@ -0,0 +1,48 @@
+
+public class Iphone implements NavegadorDeInternet, ReprodutorMusical, AparelhoTelefonico {
+
+    @Override
+    public void ligar(String numero) {
+
+    }
+
+    @Override
+    public void atender(String numero) {
+
+    }
+
+    @Override
+    public void iniciarCorreioVoz(String numero) {
+
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+
+    }
+
+    @Override
+    public void adicionaNovaAba(String url) {
+
+    }
+
+    @Override
+    public void atualizarPagina() {
+
+    }
+
+    @Override
+    public void tocar() {
+
+    }
+
+    @Override
+    public void pausar() {
+
+    }
+
+    @Override
+    public void selecionarMusica() {
+
+    }
+}
---
+++ src/NavegadorDeInternet.java
@@ -0,0 +1,5 @@
+public interface NavegadorDeInternet {
+    public void exibirPagina(String url);
+    public void adicionaNovaAba(String url);
+    public  void atualizarPagina();
+}
